188c09a1d1572af54b13e7954883f92179888a4b
peter <peter.26cast@gmail.com>
2022-11-08 18:02:57 -0500

    .

--- connect4.js
+++ connect4.js
@@ -8,41 +8,29 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
-let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
-
+let currPlayer = 1;
+let board = [];
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
   for(let y = 0; y < HEIGHT; y++){
     board.push(Array.from({length: WIDTH}));
   }
 }
 
-/** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const board = document.getElementById('board');
-  // TODO: add comment for this code
   const top = document.createElement("tr"); 
   // creates table row at the top for selecting where your chip will go
   top.setAttribute("id", "column-top");
   // adds a atribute to select by in css
   top.addEventListener("click", handleClick);
-  //add click Event listener to select which column the chip will go
-
+  // adds click Event listener to select which column the chip will go
   for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   board.append(top);
-
-  // TODO: add comment for this code
+  
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (let x = 0; x < WIDTH; x++) {
@@ -53,11 +41,9 @@ function makeHtmlBoard() {
     board.append(row);
   }
 }
-
-/** findSpotForCol: given column x, return top empty y (null if filled) */
+// Creates an X and Y coordinate on the board and when a Y coordinate is selected when the Y coordinate is selected it will place a piece at the lowest possible X then it will assign that piece and (X,Y) coordinate and the script checks when 4 are in a row it will assign a winner to which ever players pieces it is
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
   for(let y = HEIGHT - 1; y >= 0; y--){
     if(!board[y][x]){
       return y;
@@ -65,10 +51,8 @@ function findSpotForCol(x) {
   }
   return null;
 }
-/** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
   const chip = document.createElement('div');
   chip.classList.add('chip');
   chip.classList.add(`p${currPlayer}`);
@@ -76,52 +60,29 @@ function placeInTable(y, x) {
   spot.append(chip);
 }
 
-/** endGame: announce game end */
-
 function endGame(msg) {
-  // TODO: pop up alert message
   alert(msg);
 }
 
-/** handleClick: handle click of column top to play piece */
-
 function handleClick(evt) {
-  // get x from ID of clicked cell
   const x = +evt.target.id;
-
-  // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
+
   if (y === null) {
     return;
   }
-
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   board[y][x] = currPlayer;
   placeInTable(y, x);
 
-  // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
   }
-
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
   currPlayer = currPlayer === 1 ? 2 : 1;
-  // still trying to get use to this syntax over if statements. 
+  // still trying to get use to this syntax over 'if' statements. 
 }
-
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
 
 function checkForWin() {
   function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
@@ -131,8 +92,6 @@ function checkForWin() {
         board[y][x] === currPlayer
     );
   }
-
-  // TODO: read and understand this code. Add comments to help you.
 
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
